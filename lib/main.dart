d41c12b9bb602d023360465850d188fdd55e438d
Paras <khandelwalparas8@gmail.com>
2018-08-01 13:20:02 +0530

    Task Accomplished

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Uneva task',
+      title: 'Uneva Task',
       home: new HomePage(),
     );
   }
